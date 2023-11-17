7b649ce2f2a05941625b6715c89611e224ebd583
teletobi <tobias.christoph@student.uibk.ac.at>
2023-11-17 19:53:06 +0100

    movement threshold

--- Assets/Resources/Scripts/PlayerMovement.cs
+++ Assets/Resources/Scripts/PlayerMovement.cs
@@ -21,6 +21,9 @@ public class PlayerMovement : MonoBehaviour
     // speed
     public float speed;
     private float handSpeed = 1;
+    public float handSpeedThreshold = 1f;  // Adjust this value based on your requirements
+    public float smoothingFactor = 1f;
+
 
     private PhotonView photonView;
 
@@ -28,6 +31,11 @@ public class PlayerMovement : MonoBehaviour
     public GameObject xrCamera;
     public GameObject xrLeftHand;
     public GameObject xrRightHand;
+
+    public LayerMask obstacleLayer;  // Layer mask for obstacles
+
+    // Bounce-back parameters
+    public float bounceForce = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -44,6 +52,22 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         // movement stuff
+
+        // Check for obstacles using Physics.CheckBox
+        // Adjust the size and position based on your XR rig's collider size and position
+        bool isCollidingWithObstacle = Physics.CheckBox(
+            xrOrigin.transform.position,  // Center of the checkbox
+            new Vector3(0.35f, 1f, 0.35f),   // Half extents of the checkbox (adjust based on collider size)
+            forwardDirection.transform.rotation,  // Rotation of the checkbox
+            obstacleLayer  // Layer mask for obstacles
+        );
+
+        // If colliding with an obstacle, apply a bounce-back force
+        if (isCollidingWithObstacle)
+        {
+            BounceBack();
+            return;
+        }
 
         // get forward direction
         // currently from camera
@@ -63,14 +87,15 @@ public class PlayerMovement : MonoBehaviour
         var rightHandDistanceMoved = Mathf.Abs(positionPreviousRightHand.y - positionCurrentRightHand.y);
 
         // hand speed
-        handSpeed = (leftHandDistanceMoved + rightHandDistanceMoved) / 2;
-
-        if (Time.timeSinceLevelLoad > 1f)
+        handSpeed = (leftHandDistanceMoved + rightHandDistanceMoved) * 1000;
+        Debug.Log("hand:" + handSpeed);
+        if (Time.timeSinceLevelLoad > 0.01f && leftHandDistanceMoved > 0.025 && rightHandDistanceMoved > 0.025 )
         {
-            Vector3 moveAhead = forwardDirection.transform.forward * handSpeed * speed * Time.deltaTime;
-            //transform.position += moveAhead;
-            //Debug.Log(moveAhead);
-            xrOrigin.transform.position += moveAhead;
+            Vector3 moveAhead = forwardDirection.transform.forward * handSpeed / 1000 * speed * Time.deltaTime;
+            Vector3 targetPosition = xrOrigin.transform.position + moveAhead;
+
+            // Smoothly interpolate between current and target positions
+            xrOrigin.transform.position = Vector3.Lerp(xrOrigin.transform.position, targetPosition, smoothingFactor);
         }
 
         // set previous position for next frame
@@ -79,6 +104,15 @@ public class PlayerMovement : MonoBehaviour
         positionPreviousPlayer = positionCurrentPlayer;
        
     }
+
+    // Function to apply a bounce-back force
+    private void BounceBack()
+    {
+        Vector3 bounceDirection = -forwardDirection.transform.forward;  // Bounce in the opposite direction
+        Vector3 bounceForceVector = bounceDirection * bounceForce;
+        xrOrigin.transform.Translate(bounceForceVector * Time.deltaTime, Space.World);
+    }
+
 
 }
 
