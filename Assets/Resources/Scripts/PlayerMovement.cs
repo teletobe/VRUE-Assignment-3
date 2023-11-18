f8d7e1b82b70e8e8b8a7eabb42b8832d2c895936
basicasian <nana.zhang.00@gmail.com>
2023-11-18 01:34:33 +0100

    decrease threshold moving

--- Assets/Resources/Scripts/PlayerMovement.cs
+++ Assets/Resources/Scripts/PlayerMovement.cs
@@ -88,7 +88,7 @@ public class PlayerMovement : MonoBehaviour
 
         // hand speed
         handSpeed = (leftHandDistanceMoved + rightHandDistanceMoved) * 1000;
-        if (Time.timeSinceLevelLoad > 1f && leftHandDistanceMoved > 0.025 && rightHandDistanceMoved > 0.025 )
+        if (Time.timeSinceLevelLoad > 1f && leftHandDistanceMoved > 0.01 && rightHandDistanceMoved > 0.01 )
         {
             Vector3 moveAhead = forwardDirection.transform.forward * handSpeed / 1000 * speed * Time.deltaTime;
             Vector3 targetPosition = xrOrigin.transform.position + moveAhead;
